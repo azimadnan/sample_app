e2e92426e29d161641fdcdea0f73948e26707847
Azim Adnan <darkdistunnel@gmail.com>
2014-07-03 09:02:27 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
@@ -108,4 +109,9 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
-end
+  
+  describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
+  end
